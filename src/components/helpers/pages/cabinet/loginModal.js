1510d998e9b5c9836e8cc8ef9003d191ec624095
Anton <mr_tab@mail.ru>
2021-12-26 15:47:50 +0300

    new favicons, metadata, customizable referral links

--- src/components/helpers/pages/cabinet/loginModal.js
+++ src/components/helpers/pages/cabinet/loginModal.js
@@ -1,31 +1,5 @@
-import React, {Fragment} from "react";
-import {translateStr} from "../../../../utils";
-import {BodyBold, Box, Heading} from "../../global";
-import {LoginForm} from "./loginForm";
+import React from "react";
 import {closeModal} from "../../../../redux/actions";
+import {LoginDisplay} from "./loginDisplay";
 
-export const LoginModal = () => {
-    const i18n = translateStr("login");
-    const regLink = (
-        <BodyBold
-            component="a"
-            content={i18n("reg")}
-			color="brand"
-            href="https://golos.app/register?invite=graphenelab"
-            target="_blank"
-            rel="nofollow"
-        />
-    );
-
-    return(
-        <Fragment>
-            <Heading content={i18n("title")} />
-            <Box mt={.2}>
-                <BodyBold content={i18n("desc")} additionalData={{regLink}} />
-            </Box>
-            <Box mt={2.2}>
-                <LoginForm onFinish={closeModal} />
-            </Box>
-        </Fragment>
-    )
-};
+export const LoginModal = () => <LoginDisplay onFinish={closeModal} />;
--- src/components/helpers/pages/cabinet/loginScreen.js
+++ src/components/helpers/pages/cabinet/loginScreen.js
@@ -1,33 +1,12 @@
 import React from "react";
 import {EmptyLayout} from "../../../layout";
-import {Card, BodyBold, Box, Heading} from "../../global";
-import {translateStr} from "../../../../utils";
-import {LoginForm} from "./loginForm";
+import {Card} from "../../global";
+import {LoginDisplay} from "./loginDisplay";
 
-export const LoginScreen = () => {
-    const i18n = translateStr("login");
-    const regLink = (
-        <BodyBold
-            component="a"
-            content={i18n("reg")}
-            color="brand"
-            href="https://golos.app/register?invite=graphenelab"
-            target="_blank"
-            rel="nofollow"
-        />
-    );
-
-    return(
-        <EmptyLayout>
-            <Card p={4}>
-                <Heading content={i18n("title")} />
-                <Box mt={.2}>
-                    <BodyBold content={i18n("desc")} additionalData={{regLink}} />
-                </Box>
-                <Box mt={1.75}>
-                    <LoginForm />
-                </Box>
-            </Card>
-        </EmptyLayout>
-    )
-};
+export const LoginScreen = () => (
+    <EmptyLayout>
+        <Card p={4}>
+            <LoginDisplay />
+        </Card>
+    </EmptyLayout>
+);
