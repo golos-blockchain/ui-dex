f7aa2f459a63d390bca95cac4f20d82b8b134770
Anton <mr_tab@mail.ru>
2021-12-27 21:12:56 +0300

    Added link on logo

--- src/components/layout/logoDisplay.js
+++ src/components/layout/logoDisplay.js
@@ -2,12 +2,13 @@ import {connect} from "react-redux";
 import {connectSettings} from "../../redux/actions";
 import {Logo, LogoDark} from "../../svg";
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Display = props => {
     const nightMode = props.settings.nightMode;
     const Icon = nightMode ? LogoDark : Logo;
 
-    return <Icon />;
+    return <Link to="/"><Icon /></Link>;
 };
 
 export const LogoDisplay = connect(connectSettings)(Display);
